f3b62575ce3a9688b8b66847ddc9a77acc3aafd4
Aleksey Ponimash <aleksey.ponimash@rt.ru>
2026-01-25 17:35:00 +0300

    display rule

--- src/components/WordTask.vue
+++ src/components/WordTask.vue
@@ -11,6 +11,7 @@
     const task = defineModel('task',
         {type: Object, default: {count: 20, errors: 50, level: 10, tags: Array<any>}}
     );
+    const showRules = ref(false);
 
     const props = defineProps({
         title: { type: String, default: "" },
@@ -24,10 +25,14 @@
     const isSuccess = computed(() => word.value && word.value.result);
 
     watch( isSuccess, (newValue, oldValue) => {
+        if( typeof newValue == 'undefined')
+            showRules.value = false;
+
         if( typeof oldValue == "undefined" && (typeof newValue !== 'undefined') ){
-            if( newValue ) statistics.value.success++;
+            if( newValue && !showRules.value ) statistics.value.success++;
             else statistics.value.failed++;
-            emit('complete', newValue);
+
+            emit('complete', newValue && !showRules.value);
         }
     })
 
@@ -53,12 +58,13 @@
                 <div class="va-text-block" v-if="!!word?.context">
                     <span>{{ word?.context }}</span>
                 </div>
-                <div class="va-text-block" v-for="rule, index in props.rules" v-if="!inProcess && !isSuccess">
+                <div class="va-text-block" v-for="rule, index in props.rules" v-if="(!inProcess && !isSuccess) || showRules">
                     <va-divider v-if="index > 0"/>
                     <p v-for="paragraph in rule.description.split('\n')" >{{ paragraph }}</p>
                 </div>
                 <va-divider/>
                 <div class="row">
+                    <va-button preset="secondary" @click="showRules = true" :disabled="!props.rules">Правило</va-button>
                     <va-spacer/>
                     <va-button :disabled="inProcess" class="primary" icon-right="arrow_forward" v-on:click="emit('next')">Дальше</va-button>
                     <va-spacer/>
@@ -73,7 +79,13 @@
                 <va-form>
                     <va-slider label="Уровень" pins track-label-visible :min="1" :max="10" v-model="task.level"></va-slider>
                     <div class="row" style="min-height: 2vh;"></div>
-                    <va-select label="Темы" multiple :options="props.tags" text-by="desciption" value-by="id" v-model="task.tags" :max-visible-options="1" style="display: flex"></va-select>
+                    <va-select label="Темы" multiple 
+                        :options="props.tags" 
+                        text-by="desciption" 
+                        value-by="id" 
+                        v-model="task.tags" 
+                        :max-visible-options="1" 
+                        style="display: flex"/>
                     <div class="row" style="min-height: 2vh;"></div>
                     <va-slider label="Количество слов" pins track-label-visible :min="20" :max="50" :step="10" v-model="task.count"></va-slider>
                     <div class="row" style="min-height: 2vh;"></div>
